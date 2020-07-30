3fd10aa74308fcd30c92d926c449a470210c1c94
Maël Brunet <maelbrunet@gmail.com>
2020-07-30 23:59:46 +0200

    Refactoring to use jimp

--- index.js
+++ index.js
@@ -28,92 +28,91 @@ const yoshiPalette = {
 
 const greenPaletteRGB = yoshiPalette.green.map(color => hexRgb(color));
 
-// First we prepare our background
-new Jimp(140, 80, `hsla(${Math.floor(Math.random() * 360)}, 75%, 95%, 1)`, (err, image) => { image.write('./dist/background.png') });
+const imageParts = [`./img/head/head${Math.floor(Math.random() * 15) + 1}.png`, `./img/body/body${Math.floor(Math.random() * 7) + 1}.png`, `./img/feet/feet${Math.floor(Math.random() * 3) + 1}.png`, `./img/tail/tail${Math.floor(Math.random() * 5) + 1}.png`];
 
-// Then we Dr Frankenstein the different yoshi parts
-mergeImages([
-    { src: './dist/background.png' },
-    { src: `./img/head/head${Math.floor(Math.random() * 15) + 1}.png`, x: 30, y: 0 },
-    { src: `./img/body/body${Math.floor(Math.random() * 7) + 1}.png`, x: 62, y: 48 },
-    { src: `./img/feet/feet${Math.floor(Math.random() * 3) + 1}.png`, x: 62, y: 72 },
-    { src: `./img/tail/tail${Math.floor(Math.random() * 5) + 1}.png`, x: 90, y: 0 }]
-, {
-  Canvas: Canvas,
-  Image: Image
-})
-  .then(b64 => fs.writeFile('./dist/out-temp.png', b64.split(';base64,').pop(), 'base64', function(err) {
-    if (err) { console.log(err) };
+const jimps = imageParts.map(imagePart => Jimp.read(imagePart));
 
-    // Now let's paint our boy
-    const randomPaletteRGB = Object.values(yoshiPalette)[Math.floor(Math.random() * Object.keys(yoshiPalette).length)].map(color => hexRgb(color));
-    fs.createReadStream('./dist/out-temp.png')
-        .pipe(
-            new PNG({
-            filterType: 4,
-            })
-        )
-        .on('parsed', function () {
-            for (let y = 0; y < this.height; y++) {
-                for (let x = 0; x < this.width; x++) {
-                    const idx = (this.width * y + x) << 2;
+Promise.all(jimps).then(function(data) {
+  return Promise.all(jimps);
+}).then(function(data) {
+    new Jimp(140, 80, `hsla(${Math.floor(Math.random() * 360)}, 85%, 95%, 1)`, (err, image) => {
+        if (err) { console.log(err) }
+        return image
+            .composite(data[0],30,0)
+            .composite(data[1],62,48)
+            .composite(data[2],62,72)
+            .composite(data[3],90,0)
+            .write(`${__dirname}/dist/out-temp.png`);
+    });
+});
 
-                    if(
-                        this.data[idx] === greenPaletteRGB[0].red
-                        && this.data[idx + 1] === greenPaletteRGB[0].green
-                        && this.data[idx + 2] === greenPaletteRGB[0].blue
-                    ) {
-                        this.data[idx] = randomPaletteRGB[0].red;
-                        this.data[idx + 1] = randomPaletteRGB[0].green;
-                        this.data[idx + 2] = randomPaletteRGB[0].blue;
-                    } else if (
-                        this.data[idx] === greenPaletteRGB[1].red
-                        && this.data[idx + 1] === greenPaletteRGB[1].green
-                        && this.data[idx + 2] === greenPaletteRGB[1].blue
-                    ) {
-                        this.data[idx] = randomPaletteRGB[1].red;
-                        this.data[idx + 1] = randomPaletteRGB[1].green;
-                        this.data[idx + 2] = randomPaletteRGB[1].blue;
-                    } else if (
-                        this.data[idx] === greenPaletteRGB[2].red
-                        && this.data[idx + 1] === greenPaletteRGB[2].green
-                        && this.data[idx + 2] === greenPaletteRGB[2].blue
-                    ) {
-                        this.data[idx] = randomPaletteRGB[2].red;
-                        this.data[idx + 1] = randomPaletteRGB[2].green;
-                        this.data[idx + 2] = randomPaletteRGB[2].blue;
-                    }
+// Now let's paint our boy
+const randomPaletteRGB = Object.values(yoshiPalette)[Math.floor(Math.random() * Object.keys(yoshiPalette).length)].map(color => hexRgb(color));
+fs.createReadStream('./dist/out-temp.png')
+    .pipe(
+        new PNG({
+        filterType: 4,
+        })
+    )
+    .on('parsed', function () {
+        for (let y = 0; y < this.height; y++) {
+            for (let x = 0; x < this.width; x++) {
+                const idx = (this.width * y + x) << 2;
+
+                if(
+                    this.data[idx] === greenPaletteRGB[0].red
+                    && this.data[idx + 1] === greenPaletteRGB[0].green
+                    && this.data[idx + 2] === greenPaletteRGB[0].blue
+                ) {
+                    this.data[idx] = randomPaletteRGB[0].red;
+                    this.data[idx + 1] = randomPaletteRGB[0].green;
+                    this.data[idx + 2] = randomPaletteRGB[0].blue;
+                } else if (
+                    this.data[idx] === greenPaletteRGB[1].red
+                    && this.data[idx + 1] === greenPaletteRGB[1].green
+                    && this.data[idx + 2] === greenPaletteRGB[1].blue
+                ) {
+                    this.data[idx] = randomPaletteRGB[1].red;
+                    this.data[idx + 1] = randomPaletteRGB[1].green;
+                    this.data[idx + 2] = randomPaletteRGB[1].blue;
+                } else if (
+                    this.data[idx] === greenPaletteRGB[2].red
+                    && this.data[idx + 1] === greenPaletteRGB[2].green
+                    && this.data[idx + 2] === greenPaletteRGB[2].blue
+                ) {
+                    this.data[idx] = randomPaletteRGB[2].red;
+                    this.data[idx + 1] = randomPaletteRGB[2].green;
+                    this.data[idx + 2] = randomPaletteRGB[2].blue;
                 }
             }
-        
-            const dist = this.pack().pipe(fs.createWriteStream('./dist/out-final.png'));
+        }
+    
+        const dist = this.pack().pipe(fs.createWriteStream('./dist/out-final.png'));
 
-            dist.addListener('finish', () => {
-                fs.readFile('./dist/out-final.png', { encoding: 'base64' }, (err, b64) => {
-                    if (err) throw err;
+        dist.addListener('finish', () => {
+            fs.readFile('./dist/out-final.png', { encoding: 'base64' }, (err, b64) => {
+                if (err) throw err;
 
-                    // And finally we post the image to Twitter
-                    T.post('media/upload', { media_data: b64 }, function(err, data) {
-                        if (err) {
-                            console.log('error!', err);
-                        } else {
-                            console.log('tweeting the image...');
-                            
-                            T.post(
-                                'statuses/update',
-                                {
-                                    media_ids: new Array(data.media_id_string)
-                                },
-                                function(err) {
-                                    if (err){
-                                    console.log('ERROR:\n', err);
-                                    }
+                // And finally we post the image to Twitter
+                T.post('media/upload', { media_data: b64 }, function(err, data) {
+                    if (err) {
+                        console.log('error!', err);
+                    } else {
+                        console.log('tweeting the image...');
+                        
+                        T.post(
+                            'statuses/update',
+                            {
+                                media_ids: new Array(data.media_id_string)
+                            },
+                            function(err) {
+                                if (err){
+                                console.log('ERROR:\n', err);
                                 }
-                            );
-                        }
-                    });
+                            }
+                        );
+                    }
                 });
             });
         });
-    })
-);
+    });
